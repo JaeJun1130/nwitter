a3c1b78ce520a2581289e05c2fd4de698ecbf555
재준 전 <wjswowns1234@outlook.com>
2020-12-10 03:18:27 +0900

    creactUser

--- src/routes/Auth/AuthComponents.js
+++ src/routes/Auth/AuthComponents.js
@@ -1,38 +1,53 @@
+import { authService } from "fBase";
 import react, { useState, useEffect } from "react";
 import AuthPresenter from "routes/Auth/AuthPresenter";
 
 const useSet = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState(""); /*아이디*/
+    const [password, setPassword] = useState(""); /*비번*/
+    const [newAccount, setNewAcoount] = useState(true);
 
-  const onChange = (e) => {
-    const {
-      target: { name, value },
-    } = e;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-    console.log(value);
-  };
+    const onChange = (e) => {
+        const {
+            target: { name, value },
+        } = e;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+        console.log(!undefined);
+    };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-  };
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                //true creacte account
+                const data = await authService.createUserWithEmailAndPassword(email, password); //계정생성
+            } else {
+                const data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-  return { email, password, onChange, onSubmit };
+    return { email, password, onChange, onSubmit, newAccount };
 };
 const AuthComponents = () => {
-  const { email, password, onChange, onSubmit } = useSet();
-  return (
-    <AuthPresenter
-      email={email}
-      password={password}
-      onChange={(e) => onChange(e)}
-      onSubmit={(e) => onSubmit(e)}
-    />
-  );
+    const { email, password, onChange, onSubmit, newAccount } = useSet();
+    return (
+        <AuthPresenter
+            email={email}
+            password={password}
+            onChange={(e) => onChange(e)}
+            onSubmit={(e) => onSubmit(e)}
+            newAccount={newAccount}
+        />
+    );
 };
 
 export default AuthComponents;
--- src/routes/Auth/AuthPresenter.js
+++ src/routes/Auth/AuthPresenter.js
@@ -2,48 +2,41 @@ import react from "react";
 import styled from "styled-components";
 
 const LoginContainer = styled.div`
-  border: 1px solid red;
+    border: 1px solid red;
 `;
 
 const LoginButtonContainer = styled.div`
-  border: 1px solid blue;
+    border: 1px solid blue;
 `;
 
 const Login = styled.input`
-  display: flex;
+    display: flex;
 `;
 const LoginButton = styled.button``;
 
 const Form = styled.form``;
 
-const AuthPresenter = ({ email, password, onChange, onSubmit }) => {
-  return (
-    <LoginContainer>
-      <Form onSubmit={onSubmit}>
-        <Login
-          onChange={onChange}
-          type="text"
-          name="email"
-          placeholder="Email"
-          value={email}
-          required
-        ></Login>
-        <Login
-          onChange={onChange}
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={password}
-          required
-        ></Login>
-        <Login type="submit" value="Login in"></Login>
-      </Form>
-      <LoginButtonContainer>
-        <LoginButton>Continue with Google</LoginButton>
-        <LoginButton>Continue with GitHub</LoginButton>
-      </LoginButtonContainer>
-    </LoginContainer>
-  );
+const AuthPresenter = ({ email, password, onChange, onSubmit, newAccount }) => {
+    return (
+        <LoginContainer>
+            <Form onSubmit={onSubmit}>
+                <Login onChange={onChange} type="email" name="email" placeholder="Email" value={email} required></Login>
+                <Login
+                    onChange={onChange}
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={password}
+                    required
+                ></Login>
+                <Login type="submit" value={newAccount ? "회원가입" : "로그인"}></Login>
+            </Form>
+            <LoginButtonContainer>
+                <LoginButton>Continue with Google</LoginButton>
+                <LoginButton>Continue with GitHub</LoginButton>
+            </LoginButtonContainer>
+        </LoginContainer>
+    );
 };
 
 export default AuthPresenter;
