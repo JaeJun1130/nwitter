8c9a171a8a57814e7e96f52d92f837f8183653e7
j <wjswowns1234@gmail.com>
2020-12-10 17:36:52 +0900

    longin check

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fBase";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-    return (
-        <>
-            <AppRouter isLoggedIn={isLoggedIn} />
-            <footer>&copy;{new Date().getFullYear()} Nwitter</footer>
-        </>
-    );
+  const [init, setInit] = useState(false); // 로그인 상태이면 Home 화면으로 isLoggedIn true값 보내기
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인체크
+  useEffect(
+    () =>
+      authService.onAuthStateChanged((user) => {
+        if (user) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+        setInit(true);
+      }),
+    []
+  );
+  return (
+    <>
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Loding"}
+
+      <footer>&copy;{new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 }
 
 export default App;
--- src/routes/Auth/AuthComponents.js
+++ src/routes/Auth/AuthComponents.js
@@ -3,51 +3,56 @@ import react, { useState, useEffect } from "react";
 import AuthPresenter from "routes/Auth/AuthPresenter";
 
 const useSet = () => {
-    const [email, setEmail] = useState(""); /*아이디*/
-    const [password, setPassword] = useState(""); /*비번*/
-    const [newAccount, setNewAcoount] = useState(true);
+  const [email, setEmail] = useState(""); /*아이디*/
+  const [password, setPassword] = useState(""); /*비번*/
+  const [newAccount, setNewAcoount] = useState(true); /*회원가입 & 로그인*/
 
-    const onChange = (e) => {
-        const {
-            target: { name, value },
-        } = e;
-        if (name === "email") {
-            setEmail(value);
-        } else if (name === "password") {
-            setPassword(value);
-        }
-        console.log(!undefined);
-    };
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            let data;
-            if (newAccount) {
-                //true creacte account
-                const data = await authService.createUserWithEmailAndPassword(email, password); //계정생성
-            } else {
-                const data = await authService.signInWithEmailAndPassword(email, password);
-            }
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        //계정 생성
+        const data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        ); //로그인
+      } else {
+        const data = await authService.signInWithEmailAndPassword(
+          email,
+          password
+        );
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return { email, password, onChange, onSubmit, newAccount };
+  return { email, password, onChange, onSubmit, newAccount };
 };
 const AuthComponents = () => {
-    const { email, password, onChange, onSubmit, newAccount } = useSet();
-    return (
-        <AuthPresenter
-            email={email}
-            password={password}
-            onChange={(e) => onChange(e)}
-            onSubmit={(e) => onSubmit(e)}
-            newAccount={newAccount}
-        />
-    );
+  const { email, password, onChange, onSubmit, newAccount } = useSet();
+  return (
+    <AuthPresenter
+      email={email}
+      password={password}
+      onChange={(e) => onChange(e)}
+      onSubmit={(e) => onSubmit(e)}
+      newAccount={newAccount}
+    />
+  );
 };
 
 export default AuthComponents;
